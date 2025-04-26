ddbe1b2cad978062d433e8feef1178926a8e698c
Abrar Shivani <shivaniabrar@gmail.com>
2025-04-26 15:20:30 +0530

    Add request template option for http and text

--- launch/dynamo-run/src/input/http.rs
+++ launch/dynamo-run/src/input/http.rs
@@ -20,6 +20,7 @@ use dynamo_llm::{
     http::service::{discovery, service_v2},
     model_type::ModelType,
     preprocessor::OpenAIPreprocessor,
+    request_template::RequestTemplate,
     types::{
         openai::chat_completions::{
             NvCreateChatCompletionRequest, NvCreateChatCompletionStreamResponse,
@@ -39,11 +40,13 @@ pub async fn run(
     runtime: Runtime,
     flags: Flags,
     engine_config: EngineConfig,
+    template: Option<RequestTemplate>,
 ) -> anyhow::Result<()> {
     let http_service = service_v2::HttpService::builder()
         .port(flags.http_port)
         .enable_chat_endpoints(true)
         .enable_cmpl_endpoints(true)
+        .with_request_template(template)
         .build()?;
     match engine_config {
         EngineConfig::Dynamic(endpoint) => {
